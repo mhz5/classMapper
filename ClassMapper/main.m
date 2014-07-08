be37f8dda613ea91ffff38e0a2ec4ed684697f90
Michael Zhao <michael.zhao@yale.edu>
2014-07-07 22:25:17 -0700

    Change local directory

---
+++ ClassMapper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClassMapper
+//
+//  Created by Michael Zhao on 7/7/14.
+//  Copyright (c) 2014 Michael Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
